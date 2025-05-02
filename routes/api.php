ee77e29fc3598ef9b54f8f1f99895becf01d07c5
Pavel Ershov <pave1ershov@yandex.ru>
2025-05-02 20:35:29 +0300

    change users route

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,15 @@ use App\Http\Controllers\Api\V1\SavedFoodController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware(['auth:sanctum']);
-
 Route::prefix('v1')->middleware(['throttle:api'])->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
 });
 
 Route::prefix('v1')->middleware(['throttle:api', 'auth:sanctum'])->group(function () {
+    Route::get('/users', function (Request $request) {
+        return $request->user();
+    });
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::get('/saved-foods', [SavedFoodController::class, 'index']);
